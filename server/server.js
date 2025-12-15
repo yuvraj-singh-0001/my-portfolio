78cc41c96fc35ee839680ca12e4541a11eeaed99
yuvraj singh <singhyuvraj8420@gmail.com>
2025-12-15 16:42:13 +0530

    fix path of the routers

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const routes = require("./src/routes/Routes");
+const routes = require("./src/routes/routes");
 
 const app = express();
 
--- server/src/routes/Routes.js
+++ server/src/routes/Routes.js
@@ -3,7 +3,7 @@ const contactController = require("../controllers/contact");
 
 const router = express.Router();
 
-// FINAL ROUTE → POST /api/contact
+// POST /api/contact
 router.post("/contact", contactController);
 
 module.exports = router;
