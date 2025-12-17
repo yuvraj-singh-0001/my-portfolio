2bcf1ca6079f0fb53d4dcc9951bcc458c893c37e
yuvraj singh <singhyuvraj8420@gmail.com>
2025-12-17 17:36:49 +0530

    fix the gmail timeout

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -3,11 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true, // IMPORTANT
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
-  }
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 const sendMail = (options) => {
